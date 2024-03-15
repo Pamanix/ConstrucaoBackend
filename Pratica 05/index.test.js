c47eb0db6015507c07727f1ec8e1481d988ef95e
unknown <2324290078@ACADEMICO.IESB.BR>
2024-03-15 10:46:53 -0300

    adicionando atualizações incompletas da aula, ainda falta

--- Pratica 05/index.test.js
+++ Pratica 05/index.test.js
@@ -5,8 +5,20 @@ const app = require('./index');
 const request = supertest(app);
 
 test("Deve retornar status 200 e um JSON no GET", async function (){
-const response = await request.get("/produtos");
-hasUncaughtExceptionCaptureCallback(response.status).toBe(200);
-hasUncaughtExceptionCaptureCallback(response.headers['content-type']).toMatch(/json/);
+    const response = await request.get("/produtos");
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toMatch(/json/);
 });
 
+teste("Deve retornar status 200 e um JSON no GET id", async function(){
+    const response = await request get("/produtos/1");
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type'])
+    .toMatch(/json);
+});
+
+teste("Deve retornar status 404 e um JSON no GET id", async function(){
+    const response = await.request.get("/produtos/100");
+    expect(response.status).toBe(404);
+    expect(response.headers['content-type']).toMatch(/json);
+});
--- Pratica 05/router.js
+++ Pratica 05/router.js
@@ -6,5 +6,34 @@ router.get("/produtos", function(req, res){
     res.json([]);
 });
 
+router.get("/produtos/produtosId", function(req, res){
+    if(req.params.produtoId == 1){
+        res.json({});
+    } else {
+        res.status(404),json({msg:"Produto não encontrado"});
+    }
+});
+
+router.post("/produtos", function(req, res){
+    if(req.body && req.body.nome && req.body.preco){
+        res.status(201).json({});
+    } else {
+        res.status(422)
+        //.json
+    }
+});
+
+route.put("/produtos/:produtosId", function(req, res){
+    if(req.params.produtoId == 1){
+        res.json({});
+    } else {
+        res.status(404).json({msg: "Produto não encontrado"});
+    }
+});
+
+route.delete("/produtos/:produtoId", function(re, res){
+    res.status(204).end();
+});
+
 module.exports = router;
 
